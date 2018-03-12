1102308ff6e59e2c35e71e6ee99990f713521c66
alfandjacky <37285747+alfandjacky@users.noreply.github.com>
2018-03-12 15:17:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ var options = {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer Xos3j0WSqh7ymnwfhsoKIu/NzhFE8vpBQNe4D62omuU+dod5PFhyR1kgL6Gf1vgyNLhtaRtvb/7HuOes/h5y2501XVhPzGr9JG9Y5RRnVddBrzJ9qQU1T6V4cs3XVg5tgoNceGaS79IlvckV33jGdwdB04t89/1O/w1cDnyilFU=
-'   
+    'Authorization': 'Bearer Xos3j0WSqh7ymnwfhsoKIu/NzhFE8vpBQNe4D62omuU+dod5PFhyR1kgL6Gf1vgyNLhtaRtvb/7HuOes/h5y2501XVhPzGr9JG9Y5RRnVddBrzJ9qQU1T6V4cs3XVg5tgoNceGaS79IlvckV33jGdwdB04t89/1O/w1cDnyilFU='
     
   }
 }
